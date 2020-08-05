843d7337bfc7788a39d26f4f3cdaceebb14046de
Deivid Veloso <veloso.deivid@gmail.com>
2020-08-05 16:20:38 -0300

    added shared code example

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,13 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react'
+import api from '@monorepo/axios-config'
 
 const App: React.FC = () => {
-  //   useEffect(() => {
-  //     api.get("/").then((response) => {
-  //       console.log(response);
-  //     });
-  //   }, []);
+  useEffect(() => {
+    api.get('/').then(response => {
+      console.log(response)
+    })
+  }, [])
 
-  return <h1>Hello World</h1>;
-};
+  return <h1>Hello World</h1>
+}
 
-export default App;
+export default App
