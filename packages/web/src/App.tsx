9e57556bf36a4f221798cf022af285828742120c
Deivid Veloso <veloso.deivid@gmail.com>
2020-08-05 14:03:44 -0300

    added monorepo

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+const App: React.FC = () => {
+  //   useEffect(() => {
+  //     api.get("/").then((response) => {
+  //       console.log(response);
+  //     });
+  //   }, []);
+
+  return <h1>Hello World</h1>;
+};
+
+export default App;
